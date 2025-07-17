35c8afed8d6b1511cb778502730bf22c6cec286a
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-17 22:24:42 +0530

    Initialcommit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow-md px-8 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-gray-800">Kolhapur Tourism</h1>
+    <nav className="bg-blue-200 shadow-md px-8 py-4 flex justify-between items-center">
+      <h1 className="text-3xl font-bold text-blue-600">Kolhapur Tourism</h1>
       <div className="space-x-6">
         <Link to="/" className="hover:text-blue-600 font-medium">Home</Link>
         <Link to="/places" className="hover:text-blue-600 font-medium">Places</Link>
