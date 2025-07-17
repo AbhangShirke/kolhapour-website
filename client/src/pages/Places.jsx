5c4dae26372f54f8517a68b7625bc6de0dcfe338
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-17 21:13:07 +0530

    Initialcommit

---
+++ client/src/pages/Places.jsx
@@ -0,0 +1,7 @@
+export default function Places() {
+  return (
+    <div className="h-screen flex items-center justify-center text-center bg-white px-4">
+      <h1 className="text-3xl font-bold text-gray-800">Popular Places in Kolhapur (Coming Soon)</h1>
+    </div>
+  );
+}
